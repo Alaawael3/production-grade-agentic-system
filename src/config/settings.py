35f05c66eb0b1ecf18cb488b3d279d16787f5d4c
alaa wael <111670724+Alaawael3@users.noreply.github.com>
2026-08-27 13:52:44 +0300

    setting config

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,10 +1,14 @@
-from email.mime import base
+from email.policy import default
 from enum import StrEnum
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field, field_validator, model_validator
+from typing import Optional
 
-class Enviroment(StrEnum):
+
+class Environment(StrEnum):
     """Application environment types.
 
     Defines the possible environments the application can run in:
@@ -23,7 +27,25 @@ class Enviroment(StrEnum):
     TEST = "test" # Temporary DB, mocked APIs
 
 
-def get_enviroment() -> Enviroment:
+class LogLevel(StrEnum):
+    """Log level types.
+
+    Defines the possible log levels for the application.
+
+    Attributes:
+        DEBUG(str): Debug log level.
+        INFO (str): Info log level.
+        WARNING (str): Warning log level.
+        ERROR (str): Error log level.
+    """
+
+    DEBUG = "DEBUG"
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+
+
+def get_enviroment() -> Environment:
     """Get the current environment.
 
     Must be set via export APP_ENV=development | staging| production| test.
@@ -33,13 +55,13 @@ def get_enviroment() -> Enviroment:
         Environment: The current environment (development, staging, production, or test)"""
     match os.getenv("APP_ENV", "development").lower():
         case "production" | "prod":
-            return Enviroment.PRODUCTION
+            return Environment.PRODUCTION
         case "staging" | "stage":
-            return Enviroment.STAGING
+            return Environment.STAGING
         case "test" | "testing":
-            return Enviroment.TEST
+            return Environment.TEST
         case _:
-            return Enviroment.DEVELOPMENT
+            return Environment.DEVELOPMENT
 
 
 def load_env_file() -> str | Path | None:
@@ -60,3 +82,123 @@ def load_env_file() -> str | Path | None:
             return env_file
 
     return None
+
+
+ENV_FILE = load_env_file()
+
+ENV_DEFAULTS = {
+    Environment.DEVELOPMENT: {
+        "DEBUG": True,
+        "LOG_LEVEL": LogLevel.DEBUG
+    },
+
+    Environment.STAGING: {
+        "DEBUG": False,
+        "LOG_LEVEL": LogLevel.INFO
+    },
+
+    Environment.PRODUCTION: {
+        "DEBUG": False,
+        "LOG_LEVEL": LogLevel.WARNING
+    },
+
+    Environment.TEST: {
+        "DEBUG": True,
+        "LOG_LEVEL": LogLevel.DEBUG
+    }
+}
+
+class Settings(BaseSettings):
+    """Application settings configuration.
+
+    Manages application configuration with support for environment-specific
+    settings and validation of environment aliases.
+    
+    load_dotenv() and SettingsConfigDict(env_file=...) can both read the .env file, but they serve different purposes.
+
+    Think of it like this:
+    load_dotenv() -> puts values into os.environ -> os.getenv("PROJECT_NAME")
+    while: SettingsConfigDict(env_file=...) -> tells Pydantic where to get Settings values -> Settings() -> settings.PROJECT_NAME
+    """
+
+    model_config = SettingsConfigDict(env_file=ENV_FILE, env_file_encoding="utf-8", extra="ignore")
+
+    APP_ENV: Environment = Field(...)
+    PROJECT_NAME: str = Field(..., max_length=100)
+    VERSION: str = Field(...)
+    DEBUG: Optional[bool] = Field(default=None)
+    PROJECT_ROOT: str = Field(...)
+    LOG_LEVEL: Optional[str] = Field(default=None)
+
+    @model_validator(mode="after")
+    def configure_environment_defaults(self):
+        """After Pydantic has loaded and validated my settings, 
+        fill in any missing settings with defaults appropriate for the current environment.
+
+        if getattr(self, key, None) is None:
+        This asks: Does the Settings object already have a value for this setting?
+        For example: self.DEBUG = None
+        Then: getattr(self, "DEBUG", None)
+        returns: None
+        Therefore: None is None is True.
+        So it sets the default.
+        Then this setattr(self, key, value)
+        means: Set this attribute on the Settings object.
+        For example:
+        key = "DEBUG"
+        value = False
+        becomes effectively: self.DEBUG = False
+
+        Returns:
+            Settings: The settings instance with environment defaults applied.
+        """
+        current_env_defaults = ENV_DEFAULTS.get(self.APP_ENV, {})
+
+        for key, value in current_env_defaults.items():
+            if getattr(self, key, None) is None: # Does the Settings object already have a value for this setting? self.DEBUG = None
+                setattr(self, key, value)
+
+        return self
+
+    @field_validator("APP_ENV", mode="before") # apply before pydantic validation
+    @classmethod
+    def normalize_environment(cls, value: str) -> str:
+        """Normalize APP_ENV aliases to supported environment values
+
+        Args:
+            value: The input environment value to normalize.
+
+        Returns:
+            str: The normalized environment value.
+        """
+        aliases = {
+            "dev": "development",
+            "development": "development",
+            "stage": "staging",
+            "staging": "staging",
+            "prod": "production",
+            "production": "production",
+            "test": "test",
+            "testing": "test"
+        }
+
+        normalized_env = aliases.get(str(value).lower())
+        if normalized_env is None:
+            raise ValueError(
+                f"Invalid APP_ENV '{value}'"
+                f"Expected one of: {','.join(sorted(aliases.keys()))}"
+            )
+        return normalized_env
+
+
+settings = Settings()
+
+def main():
+    print(f"welcome from `{os.path.basename(__file__).split('.')[0]}` modeul, nothing to do ^___^!")
+    for key, value in settings.model_dump().items():
+        print(f"{key}: {value}")
+
+
+if __name__ == "__main__":
+    main()
+
